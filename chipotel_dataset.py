a817f6cbdf93ef64f0af71fc60508595243574fe
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-05-04 07:44:38 +0530

    commit

--- chipotel_dataset.py
+++ chipotel_dataset.py
@@ -27,15 +27,16 @@ c=c.sort_values(['quantity'], ascending=False)
 c.head(1)
 c.head()
 
-
 df.item_price.dtype
 # change item price type
+
 dollarizer=lambda x:float(x[1:-1])
 df.item_price=df.item_price.apply(dollarizer)
 
 df.item_price.dtype
 
 # Revenue
+
 revenue=(df['quantity']*df['item_price']).sum()
 print(f'Revenue was: ${str(np.round(revenue,2))}')
 
@@ -48,6 +49,7 @@ order_grouped=df.groupby(by=['order_id']).sum()
 order_grouped.mean()['revenue']
 
 # OR
+
 df.groupby(by=['order_id']).sum().mean()['revenue']
 
 # Number of different item sold
